995132c25286dbc6457c8f1cfce4cdbe4bab11a9
Haris Michelogiannakis <tmichelogiannakis@gmail.com>
2021-03-28 12:36:26 +0300

    feat: connect to the blockchain

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,60 @@
-import { ChakraProvider, Container } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+import Web3 from 'web3';
+import {
+  ChakraProvider,
+  Box,
+  ListItem,
+  OrderedList,
+  UnorderedList
+} from '@chakra-ui/react';
 import theme from './theme';
 
 const App = (): JSX.Element => {
+  const [network, setNetwork] = useState<string>('');
+  const [accounts, setAccounts] = useState<string[]>([]);
+
+  const loadBlockchainDate = async () => {
+    if (Web3.givenProvider) {
+      const web3 = new Web3(Web3.givenProvider);
+      const network = await web3.eth.net.getNetworkType();
+      setNetwork(network);
+      const accounts = await web3.eth.getAccounts();
+      setAccounts(accounts);
+    }
+  };
+
+  useEffect(() => {
+    loadBlockchainDate();
+  }, []);
+
   return (
     <ChakraProvider theme={theme}>
-      <Container>Hello world</Container>
+      <Box
+        height="100vh"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <UnorderedList>
+          {network && (
+            <ListItem>
+              Network: <strong>{network}</strong>
+            </ListItem>
+          )}
+          {accounts && accounts.length && (
+            <ListItem>
+              Accounts:
+              <OrderedList pl="8">
+                {accounts.map(account => (
+                  <ListItem>
+                    <strong>{account}</strong>
+                  </ListItem>
+                ))}
+              </OrderedList>
+            </ListItem>
+          )}
+        </UnorderedList>
+      </Box>
     </ChakraProvider>
   );
 };
